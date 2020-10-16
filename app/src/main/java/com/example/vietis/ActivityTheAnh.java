6a53ece87b17ef5f0acf823bf57c0ba93382c2c3
AnhNT <timer217@gmail.com>
2020-10-16 15:24:54 +0700

    Commit
    First commit

---
+++ app/src/main/java/com/example/vietis/ActivityTheAnh.java
@@ -0,0 +1,14 @@
+package com.example.vietis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTheAnh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_anh);
+    }
+}
