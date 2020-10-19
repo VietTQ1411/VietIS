73739d00075e57c2162d00a76ccf143580516b5d
Huy Pham <huypham1700@gmail.com>
2020-10-19 15:12:23 +0700

    login screen, main screen design

---
+++ app/src/main/java/com/example/vietis/activities/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.vietis.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.vietis.R;
+import com.example.vietis.common.IActivity;
+
+public class MainActivity extends AppCompatActivity implements IActivity {
+    private Button btnFacebook;
+    private Button btnGoogle;
+    private Button btnLogin;
+    private TextView signUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mappingUI();
+        setupAction();
+    }
+
+    @Override
+    public void mappingUI() {
+        btnFacebook = findViewById(R.id.main_facebook);
+        btnGoogle = findViewById(R.id.main_google);
+        btnLogin = findViewById(R.id.main_login);
+        signUp = findViewById(R.id.main_sign_up);
+    }
+
+    @Override
+    public void setupAction() {
+     btnLogin.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+             MainActivity.this.startActivity(intent);
+         }
+     });
+    }
+}
