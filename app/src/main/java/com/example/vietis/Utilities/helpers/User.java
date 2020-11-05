f8199701e66f6b90499fb167a3fe311806d688d0
VietTQ1411 <46323176+VietTQ1411@users.noreply.github.com>
2020-11-05 12:25:31 +0700

    m

---
+++ app/src/main/java/com/example/vietis/Utilities/helpers/User.java
@@ -0,0 +1,6 @@
+package com.example.vietis.Utilities.helpers;
+
+public class User {
+    public static final com.example.vietis.Data.entity.User user =
+             com.example.vietis.Data.entity.User.builder().build();
+}
