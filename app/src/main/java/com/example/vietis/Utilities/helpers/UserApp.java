60490bad11622cb959c1277139b6951aeb7cb1e4
VietTQ1411 <46323176+VietTQ1411@users.noreply.github.com>
2020-11-05 12:26:23 +0700

    Update UserApp.java

--- app/src/main/java/com/example/vietis/Utilities/helpers/UserApp.java
+++ app/src/main/java/com/example/vietis/Utilities/helpers/UserApp.java
@@ -1,6 +1,6 @@
 package com.example.vietis.Utilities.helpers;
 
-public class User {
+public class UserApp {
     public static final com.example.vietis.Data.entity.User user =
              com.example.vietis.Data.entity.User.builder().build();
 }
