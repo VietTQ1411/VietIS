a18be26f4f2109df9bb6d6e1b7bc698fd0167ac2
TungNT. Tung Nguyen Thanh <TungNT@vietis.vn>
2020-12-02 16:01:39 +0700

    asdsad
    asdsad

--- app/src/main/java/com/example/vietis/activities/Home/ui/dashboard/EditProfileActivity.java
+++ app/src/main/java/com/example/vietis/activities/Home/ui/dashboard/EditProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.vietis.activities.Home.ui.dashboard;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -22,20 +21,13 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
     private ImageButton ibEditPassword;
     private ImageButton ibEditName;
     private ImageButton ibEditPhoneNumber;
-    private ImageButton ibEditAddress;
-    private Button btnSavePassword;
-    private Button btnSaveName;
-    private Button btnSavePhoneNumber;
-    private Button btnSaveAddress;
     private EditText edtEmail;
     private EditText edtPassword;
     private EditText edtName;
     private EditText edtPhoneNumber;
-    private EditText edtAddress;
     private EditText edtNewPassword;
     private EditText edtNewName;
     private EditText edtNewPhoneNumber;
-    private EditText edtNewAddress;
     private EditText edtCurrentPassword;
     private EditText edtConfirmPassword;
     private TextView txtWhy;
@@ -43,7 +35,6 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
     private LinearLayout llPassword;
     private LinearLayout llName;
     private LinearLayout llPhoneNumber;
-    private LinearLayout llAddress;
 
     private Boolean isVisible = true;
 
@@ -64,27 +55,19 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
         ibEditPassword = findViewById(R.id.ibEditPassword);
         ibEditName = findViewById(R.id.ibEditName);
         ibEditPhoneNumber = findViewById(R.id.ibEditPhoneNumber);
-        ibEditAddress = findViewById(R.id.ibEditAddress);
-        btnSavePassword = findViewById(R.id.btnSavePassword);
-        btnSaveName = findViewById(R.id.btnSaveName);
-        btnSavePhoneNumber = findViewById(R.id.btnSavePhoneNumber);
-        btnSaveAddress = findViewById(R.id.btnSaveAddress);
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         edtName = findViewById(R.id.edtName);
         edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
-        edtAddress = findViewById(R.id.edtAddress);
         edtNewPassword = findViewById(R.id.edtNewPassword);
         edtNewName = findViewById(R.id.edtNewName);
         edtNewPhoneNumber = findViewById(R.id.edtNewPhoneNumber);
-        edtNewAddress = findViewById(R.id.edtNewAddress);
         edtCurrentPassword = findViewById(R.id.edtCurrentPassword);
         edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
         txtWhy = findViewById(R.id.txtWhy);
         txtWhy1 = findViewById(R.id.txtWhy1);
         settingActivityViewModel = new SettingActivityViewModel();
         llPassword = findViewById(R.id.llPassword);
-        llAddress = findViewById(R.id.llAddress);
         llName = findViewById(R.id.llName);
         llPhoneNumber = findViewById(R.id.llPhoneNumber);
     }
@@ -92,6 +75,7 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
     @Override
     public void setupUI(){
         getSettingData();
+
         ibEditPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,6 +87,7 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
                 }
             }
         });
+
         ibEditName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -114,6 +99,7 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
                 }
             }
         });
+
         ibEditPhoneNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -125,17 +111,7 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
                 }
             }
         });
-        ibEditAddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isVisible = !isVisible;
-                if(isVisible){
-                    llAddress.setVisibility(View.VISIBLE);
-                }else{
-                    llAddress.setVisibility(View.GONE);
-                }
-            }
-        });
+
         if(!edtNewPassword.getText().equals(edtConfirmPassword.getText())){
             txtWhy1.setText("New Password doesnt match. Please try again");
             txtWhy1.setVisibility(View.VISIBLE);
@@ -148,34 +124,7 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
         }else{
             txtWhy.setVisibility(View.GONE);
         }
-        btnSavePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserApp.user.setPassword(String.valueOf(edtNewPassword.getText()));
-                edtPassword.setHint(UserApp.user.getPassword());
-            }
-        });
-        btnSaveName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserApp.user.setName(String.valueOf(edtNewName.getText()));
-                edtName.setHint(UserApp.user.getName());
-            }
-        });
-        btnSavePhoneNumber.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserApp.user.setPhoneNumber(String.valueOf(edtNewPhoneNumber.getText()));
-                edtPhoneNumber.setHint(UserApp.user.getPhoneNumber());
-            }
-        });
-        btnSaveAddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserApp.user.setAddress(String.valueOf(edtNewAddress.getText()));
-                edtAddress.setHint(UserApp.user.getAddress());
-            }
-        });
+
     }
 
     public void getSettingData(){
@@ -190,6 +139,5 @@ public class EditProfileActivity extends AppCompatActivity implements IView {
         edtPassword.setHint(UserApp.user.getHashedPassword());
         edtName.setHint(UserApp.user.getName());
         edtPhoneNumber.setHint(UserApp.user.getPhoneNumber());
-        edtAddress.setHint(UserApp.user.getAddress());
     }
 }
